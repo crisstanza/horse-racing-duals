baad9c598bfae41cfc96576e019ae273b69d7ef7
Cristiano Neves <cristiano.neves@datacom.ind.br>
2014-07-08 12:52:40 -0300

    Test script support.

---
+++ src/Solution.java
@@ -0,0 +1,13 @@
+// Read inputs from System.in, Write outputs to System.out.
+// Your class name has to be Solution
+
+import java.util.Scanner;
+
+class Solution {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(1);
+    }
+}
---
+++ test-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Run this script to easily test your solution
+test=1
+
+# Uncomment these next lines to test other inputs!
+# test=2
+# test=3
+
+clear
+
+ACTUAL=`java -cp bin Solution < in${test}.txt`
+EXPECTED=`cat out${test}.txt`
+[ ${ACTUAL} == ${EXPECTED} ] && SUCCESS=1 || SUCCESS=0
+
+echo ''
+echo 'actual  : '${ACTUAL}
+echo 'expected: '${EXPECTED}
+echo ''
+[ ${SUCCESS} == 1 ] && echo 'Success!' || echo 'Fail!'
+echo ''
